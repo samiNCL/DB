82e8e826d6fc0309b8a8584a2f3dbc92dec0c5e7
samiNCL <60569888+samiNCL@users.noreply.github.com>
2020-02-21 21:43:08 +0000

    initial database tables
    tables for customers and admins

---
+++ sql/admin.sql
@@ -0,0 +1,12 @@
+CREATE TABLE SageFeedback.admin (
+	`no` BIGINT auto_increment NOT NULL,
+	firstName TEXT NOT NULL,
+	sureName TEXT NOT NULL,
+	email TEXT NOT NULL,
+	password TEXT NOT NULL,
+	userName TEXT NOT NULL,
+	CONSTRAINT admin_PK PRIMARY KEY (`no`)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_0900_ai_ci;
